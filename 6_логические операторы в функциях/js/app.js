346d9399b551da2647be67e9564c6855107d35ed
Mershana <102474997+Mershana@users.noreply.github.com>
2022-06-24 17:38:41 +0300

    6_логические операторы в функциях

---
+++ 6_логические операторы в функциях/js/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/* Логические операторы без if в функциях JavaScript */
+/* Задача 1⋕js.Pm.UF.LO.1
+Дана следующая функция:
+function func(a, b) {
+	if (a == b) {
+		return true;
+	} else {
+		return false;
+	}
+}
+Перепишите ее код в сокращенной форме согласно изученной теории. */
+//RESHENIE
+/* function func(a, b){
+	return a == b;
+} */
+
+/* Задача 2⋕js.Pm.UF.LO.2
+Дана следующая функция:
+function func(a, b) {
+	if (a != b) {
+		return true;
+	} else {
+		return false;
+	}
+} */
+//RESHENIE
+/* function func(a, b){
+	return a != b;
+} */
+
+/* Задача 3⋕js.Pm.UF.LO.3
+Дана следующая функция:
+function func(a, b) {
+	if (a + b >= 10) {
+		return true;
+	} else {
+		return false;
+	}
+} */
+//RESHENIE
+/* function func(a, b){
+	return (a + b) >= 10;
+} */
+
+/* Задача 4⋕js.Pm.UF.LO.4
+Дана следующая функция:
+function func(num) {
+	if (num >= 0) {
+		return true;
+	} else {
+		return false;
+	}
+} */
+//RESHENIE
+/* function func(num){
+	return num >= 0;
+} */
